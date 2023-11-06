0f4e2d23525f6f6020234a21e415cc2cee47f00f
Tanuki <95048257+TanukiTheRacoon@users.noreply.github.com>
2023-11-06 19:04:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,54 +1,47 @@
 #include <iostream>
 #include <windows.h>
-#include <cstdlib>
-
+#include <locale.h>
 using namespace std;
+int punkty;
+void pytanie(string pyt,string odp,string podp,int time){
+    cout<<pyt<<endl;
 
-string imie;
-string odp;
-
-int main()
-{
-    //https://cpp0x.pl/kursy/Kurs-WinAPI-C++/Roznosci/Kolory-w-konsoli/374
-    HANDLE uchwyt = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(uchwyt, 12);
-    cout << "\aPodaj imie: ";
-    cin >> imie;
-    int pkt = 0;
-
-    system("cls");
-    cout<< "3";
-    Sleep(1000);
-    system("cls");
-    cout<< "3..2";
-    Sleep(1000);
-    system("cls");
-    cout<< "3..2..1..";
-    Sleep(1000);
-    system("cls");
-
-    cout << imie << " witaj w tescie z programowania\n";
-
-    cout << "Co to jest C++?"<<endl;
-    cout << "a) Piosenkarka"<<endl;
-    cout << "b) Jezyk programowania"<<endl;
-    cout << "c) Mlynek do herbaty"<<endl;
-    cout << "Twoja odpowiedz: ";
-    cin >> odp;
-    if(odp == "b")
-    {
-        cout << "Poprawna odpowiedz!";
-        pkt++;
+    cout<<"Podaj odpowiedz:";
+    cin>>odp;
+    if(odp == podp){
+        cout<<"Dobra odpowiedz!";
+        punkty++;
     }
-    else
-    {
-        cout << "Zle! Jest to jezyk programowania!";
-
+    else{
+        cout<<"zla odpowiedz poprawna odp:" + podp;
     }
-    Sleep(3000);
-    system("cls");
+    Sleep(time);
+    system("CLS");
+}
+bool endsWith(
+            const string str,
+            const string subStr) {
+            size_t strLen = str.length();
+            size_t subStrLen = subStr.length();
 
-
-
+            return  strLen >= subStrLen &&
+                str.compare(strLen - subStrLen, subStrLen, subStr) == 0;
+}
+int main(){
+    setlocale(LC_CTYPE, "Polish");
+    string imie;
+    cout<<"Podaj imie:";
+    cin>>imie;
+    pytanie("W jakim roku konosla sega mega drive/genesis miała premiere w USA?\na)1989\nb)1970\nc)2000\nd)2001","","a",6000);
+    pytanie("Jaka byla najlepiej sprzedająca sie gra na sega mega drive \na)Street fighter II\nb)super mario bros\nc)Sonic the Hedgehog 2\nd)The legend of Zelda","","c",6000);
+    pytanie("Jakie moduły wyszły do sega genesis\na)sega cd\nb)sega 16x\nc)sega 64x\nd)sega powerhouse","","a",6000);
+    pytanie(" Co takiego posiadała konsola segi, czcego brakowało konsoli nintendo?\na)Blast Processing\nb)antialiasing\nc)fidelityFX\nd)DLSS","","a",6000);
+    pytanie("Czy kartridże do Segi Mega Drive(japońskie wydanie) pasowały do segi genesis(Amerykańskie wydanie)\na)Tak\nb)Nie","","b",6000);
+    if(endsWith(imie,"a")){
+        cout<<"Gratulacje " << imie <<",zdobyłaś "<<punkty<<"/5 punktów";
+    }
+    else{
+        cout<<"Gratulacje " << imie <<",zdobyłeś "<<punkty<<"/5 punktów";
+    }
     return 0;
 }
